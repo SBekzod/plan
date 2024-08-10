81755e8c225da4cb09fa1c8724eab4f14988e2d1
martin <martin@comets.kr>
2024-08-10 21:45:58 +0900

    fix: modify final

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 const moment = require("moment");
-const counter = require("../parker/calculate");
 
-const time = moment().format("HH:mm:ss");
-console.log("Executed at", time);
-
-const result = counter.add(10, 40);
-console.log("result:", result);
