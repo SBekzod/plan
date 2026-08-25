b0424aedffa5b96f8b8aca4b37372623e95ae37d
martin <martin@comets.kr>
2026-08-25 22:33:38 +0900

    feat: Learning git commands

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("GIT COMMANDS");
+
+/*
+
+git init
+
+git add .
+git status
+git commit -m "feat/fix: MESSAGE"
+
+git remote add origin HTTPS
+git remote 
+git push origin master
+git pull origin master 
+
+*/
